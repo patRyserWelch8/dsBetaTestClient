9cbced06d3de3abe22a6b315996bd6438e870435
Stuart Wheater <stuart.wheater@arjuna.com>
2019-05-06 23:09:39 +0100

    Fix GLM test

--- tests/testthat/test-smk-ds.glm-2.o.R
+++ tests/testthat/test-smk-ds.glm-2.o.R
@@ -26,13 +26,16 @@ connect.smk.dataset.sim(list("LAB_TSC", "LAB_TRIG"))
 
 context("dsBetaTestClient::ds.glm.o(): Standard Gaussian regression model for piecewise exponential regression analysis:smoke")
 
-mod.D<-ds.glm.o('D$LAB_TSC~D$LAB_TRIG',family="gaussian")
-output.D<-c(mod.D$coefficients[,1],mod.D$coefficients[,2])
+mod.D <- ds.glm.o('D$LAB_TSC~D$LAB_TRIG',family="gaussian")
+output.D1 <- mod.D$coefficients[,1]
+output.D2 <- mod.D$coefficients[,2]
 
-output.R<-NULL
+output.R1 <- c('(Intercept)' = 5.697114181, 'D$LAB_TRIG' = 0.075299766)
+output.R2 <- c('(Intercept)' = 0.019983645, 'D$LAB_TRIG' = 0.007754159)
 
 test_that("glm_gaussian", {
-    expect_equal(ds.ls(datasources=ds.test_env$connection.opal)$sim1[2],output.D,output.R)
+    expect_equal(output.D1, output.R1)
+    expect_equal(output.D2, output.R2)
 })
 
 #
