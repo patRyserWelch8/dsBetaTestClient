9521e7cfda0f75200814d8c2bdaadd0cc12c0c1f
Stuart Wheater <stuart.wheater@arjuna.com>
2019-05-24 23:09:38 +0100

    Initial 'ds.asInteger.o' smoke test

---
+++ tests/testthat/test-smk-ds.asInteger.o.R
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2018 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+context("dsBetaTestClient::ds.asInteger.o:smoke")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("GENDER"))
+
+#
+# Tests
+#
+
+context("dsBetaTestClient::ds.asInteger.o():smoke simple test")
+
+test_that("simple test", {
+    res <- ds.asInteger.o("D$GENDER")
+
+    expect_equal(length(res), 2)
+    expect_equal(res$is.object.created, "A data object <D$LAB_TSC.char> has been created in all specified data sources")
+    expect_equal(res$validity.check, "<D$LAB_TSC.char> appears valid in all sources")
+})
+
+#
+# Done
+#
+
+context("dsBetaTestClient::ds.asInteger.o 1:smoke done")
