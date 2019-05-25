23eba7a8b92b3f9f242c1cfa79601ee86824ac53
Stuart Wheater <stuart.wheater@arjuna.com>
2019-05-25 11:47:38 +0100

    Commented out console messages for debugging and process indication

--- tests/testthat/test-ds_as_factor.o.R
+++ tests/testthat/test-ds_as_factor.o.R
@@ -7,8 +7,8 @@ test_that("combined data set",
     connect.all.datasets()
     factor.from.file <- list(levels(as.factor(ds.test_env$local.values[,15])))
     factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
-    print(class(factor.from.server[1]))
-    print(class(factor.from.file))
+#    print(class(factor.from.server[1]))
+#    print(class(factor.from.file))
     expect_true(length(setdiff(factor.from.file, factor.from.server[1]))==0)
 })
 
@@ -17,8 +17,8 @@ test_that("split dataset",
     connect.dataset.1()
     factor.from.file <- list(levels(as.factor(ds.test_env$local.values.1[,15])))
     factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
-    print(class(factor.from.server[1]))
-    print(class(factor.from.file))
+#    print(class(factor.from.server[1]))
+#    print(class(factor.from.file))
     expect_true(length(setdiff(factor.from.file, factor.from.server[1]))==0)
     
     connect.dataset.2()
@@ -29,8 +29,8 @@ test_that("split dataset",
     connect.dataset.3()
     factor.from.file <- list(levels(as.factor(ds.test_env$local.values.3[,15])))
     factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
-    print(class(factor.from.server[1]))
-    print(class(factor.from.file))
+#    print(class(factor.from.server[1]))
+#    print(class(factor.from.file))
     expect_true(length(setdiff(factor.from.file, factor.from.server[1]))==0)
 })
 
