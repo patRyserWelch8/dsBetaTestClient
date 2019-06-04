40ca390fe76e3c8bf240c514ee7fda0cefe7bffa
Stuart Wheater <stuart.wheater@arjuna.com>
2019-06-04 14:11:48 +0100

    Unified password

--- tests/testthat/connection_to_datasets/login_details.R
+++ tests/testthat/connection_to_datasets/login_details.R
@@ -21,9 +21,9 @@ ds.test_env <- new.env()
   ds.test_env$user_2 <- "administrator"
   ds.test_env$user_3 <- "administrator"
 
-  ds.test_env$password_1 <- "password"
-  ds.test_env$password_2 <- "password"
-  ds.test_env$password_3 <- "password"
+  ds.test_env$password_1 <- "datashield_test&"
+  ds.test_env$password_2 <- "datashield_test&"
+  ds.test_env$password_3 <- "datashield_test&"
   ds.test_env$secure_login_details = TRUE
 
 
