e48edfb37cf1e82eb7a0e671c2dbf922f49d016e
PatRyserWelch8 <patricia.ryser-welch@newcastle.ac.uk>
2019-06-06 15:46:44 +0100

    recodeValues progress

--- tests/testthat/test-recodeValues_o.R
+++ tests/testthat/test-recodeValues_o.R
@@ -1,11 +1,32 @@
 source("connection_to_datasets/init_all_datasets.R")
 
+
+
 context("<START>ds.recodeValues.o</START>")
 context("Returns expected numerical value")
 test_that("combined data set",
 {
    connect.all.datasets()
-   factor.from.file <- list(levels(as.factor(ds.test_env$local.values[,15])))
-   factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
-   expect_true(length(setdiff(factor.from.file, factor.from.server[1]))==0)
+   #change value locally start
+   row <- sample(nrow(ds.test_env$local.values),1) 
+   value <- ds.test_env$local.values[row,6] 
+   print(ds.test_env$local.values[row,6])
+   print(value)
+   ds.test_env$local.values[row,6] <- 0
+   print(ds.test_env$local.values[row,6])
+   print(value)
+   #change value locally end
+   #change value on the server
+   #a <- ds.recodeValues.o('D$FACTOR_INTEGER',c(0,1,2),c(20,27,37),newobj = 'INTEGER_recoded',datasources = connection.opal)
+   recode.from.server <- ds.recodeValues.o('D$INTEGER',value,c(0),newobj = 'INTEGER_recoded',datasources = ds.test_env$connection.opal)
+   factor.from.server <- ds.asFactor.o('INTEGER_recoded','INT.f', datasources = ds.test_env$connection.opal)
+   factors <- factor.from.server[[1]]
+   print(factors)
+   
+   print(length(factors[factors == "0"])> 0)
+   expect_true(length(factors[factors == "0"])> 0)
+   
 })
+
+
+context("<END>ds.recodeValues.o</END>")
