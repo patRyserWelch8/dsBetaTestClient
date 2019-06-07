13db0dabe0554ef14a2b0adabdba8da72a21ca3d
OllyButters <oliver.butters@gmail.com>
2019-06-07 22:24:41 +0100

    changed format of contexts

--- tests/testthat/test-ds.as.factor.o.R
+++ tests/testthat/test-ds.as.factor.o.R
@@ -1,7 +1,8 @@
 source("connection_to_datasets/init_all_datasets.R")
 
-context("<START>ds.as_factor.o</START>")
-context("Returns expected numerical value")
+#context("<START>ds.as_factor.o</START>")
+#context("Returns expected numerical value")
+context("ds.as.factor()::mathematical::Returns expected numerical value")
 test_that("combined data set",
 {
     connect.all.datasets()
@@ -16,12 +17,12 @@ test_that("split dataset",
     factor.from.file <- list(levels(as.factor(ds.test_env$local.values.1[,15])))
     factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
     expect_true(length(setdiff(factor.from.file, factor.from.server[1]))==0)
-    
+
     connect.dataset.2()
     factor.from.file <- list(levels(as.factor(ds.test_env$local.values.2[,15])))
     factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
     expect_true(length(setdiff(factor.from.file, factor.from.server[1]))==0)
-    
+
     connect.dataset.3()
     factor.from.file <- list(levels(as.factor(ds.test_env$local.values.3[,15])))
     factor.from.server <- ds.asFactor.o('D$FACTOR_INTEGER','FACTOR_INT.f', datasources = ds.test_env$connection.opal)
@@ -31,15 +32,15 @@ test_that("split dataset",
 #context("Returns expected non-numerical values")
 #test_that("character",
 #{
-#  
+#
 #  connect.all.datasets()
 #  factor.from.file <- list(levels(as.factor(ds.test_env$local.values[,16])))
 #  factor.from.server <- ds.asFactor.o('D$FACTOR_CHARACTER','FACTOR_CHAR.f', datasources = ds.test_env$connection.opal)
 #  expect_true(true)
-  
+
 #})
 
-context("Mathematical properties")
+#context("Mathematical properties")
 test_that("every factor should be unique",
 {
   connect.all.datasets()
@@ -47,8 +48,4 @@ test_that("every factor should be unique",
   factors.vector <- unlist(factor.from.server[1])
   expect_true(!any(duplicated(factors.vector)))
 })
-context("<END>ds.as_factor.o</END>")
-
-
-
-
+#context("<END>ds.as_factor.o</END>")
