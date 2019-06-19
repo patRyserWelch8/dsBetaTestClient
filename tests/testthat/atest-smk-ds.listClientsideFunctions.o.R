c1fd08118be9796c04c05440250ed01e5d90cd39
Stuart Wheater <stuart.wheater@arjuna.com>
2019-06-19 03:18:11 +0100

    Initial 'ds.listClientsideFunctions.o' test

---
+++ tests/testthat/atest-smk-ds.listClientsideFunctions.o.R
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2019 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+# context("dsBetaTestClient::ds.listClientsideFunctions.o:smoke")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("LAB_TSC", "LAB_HDL"))
+
+#
+# Tests
+#
+
+context("ds.listClientsideFunctions.o()::smoke::check results")
+test_that("check results", {
+
+    res <- ds.listClientsideFunctions.o()
+
+    print("======")
+    print(res)
+    print("======")
+
+    expect_equal(res, "test", fixed=TRUE)
+})
+
+#
+# Done
+#
+
+# context("dsBetaTestClient::ds.listClientsideFunctions.o:smoke done")
