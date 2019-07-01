c43b80a43adb86acf377be8697b37ea99988d9e9
Stuart Wheater <stuart.wheater@arjuna.com>
2019-07-01 11:53:06 +0100

    Fixed argument checking, plus test

---
+++ tests/testthat/test-arg-ds.length.o.R
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2018 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+# context("dsBetaTestClient::ds.length.o:args test")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("LAB_TSC"))
+
+#
+# Tests
+#
+
+context("ds.length.o::arg::test errors")
+test_that("length_erros", {
+    ds.asCharacter.o(x='D$LAB_TSC', newobj="not_a_numeric")
+
+    expect_error(ds.length.o(), "Please provide the name of the input vector!", fixed=TRUE)
+    expect_error(ds.length.o(x='D$LAB_TSC', type='datashield'), 'Function argument "type" has to be either "both", "combine" or "split"', fixed=TRUE)
+#    expect_error(ds.length.o(x='not_a_numeric'), "The input object must be an integer or a numeric vector.", fixed=TRUE)
+    expect_error(ds.length.o(check=TRUE), "Please provide the name of the input vector!", fixed=TRUE)
+    expect_error(ds.length.o(x='D$LAB_TSC', type='datashield', check=TRUE), 'Function argument "type" has to be either "both", "combine" or "split"', fixed=TRUE)
+#    expect_error(ds.length.o(x='not_a_numeric', check=TRUE), "The input object must be an integer or a numeric vector.", fixed=TRUE)
+})
+
+#
+# Done
+#
+
+# context("dsBetaTestClient::ds.length.o:arg done")
