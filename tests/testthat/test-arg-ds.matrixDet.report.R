fbf1355f58e2ffedfb589fceca925fab0fc56950
Stuart Wheater <stuart.wheater@arjuna.com>
2019-07-04 16:45:34 +0100

    Factor out 'arg' tests

---
+++ tests/testthat/test-arg-ds.matrixDet.report.R
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2019 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnurg/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+# context("dsBetaTestClient::ds.matrixDet.report::arg")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("LAB_TSC"))
+
+#
+# Tests
+#
+
+context("ds.matrixDet.report::arg::test errors")
+
+test_that("ds.matixDet.report erros", {
+    expect_error(ds.matrixDet.report(M1=NULL), "Error: Please provide the name of the matrix representing M1", fixed=TRUE)
+})
+
+#
+# Tear down
+#
+
+# context("dsBetaTestClient::ds.matrixDet.report::arg done")
