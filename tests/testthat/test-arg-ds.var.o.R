daa856729d9565c01ab86e72218b8f78317380ae
Stuart Wheater <stuart.wheater@arjuna.com>
2019-07-04 17:01:43 +0100

    Update arg tests

--- tests/testthat/test-arg-ds.var.o.R
+++ tests/testthat/test-arg-ds.var.o.R
@@ -1,19 +1,19 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2014 OBiBa,
-#               2019 University of Newcastle upon Tyne. All rights reserved.
+#               2018 University of Newcastle upon Tyne. All rights reserved.
 #
 # This program and the accompanying materials
 # are made available under the terms of the GNU Public License v3.0.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnurg/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------------------
 
 #
 # Set up
 #
 
-# context("dsBetaTestClient::ds.matrixDet.report::arg")
+# context("dsBetaTestClient::ds.var.o::args test")
 
 source("connection_to_datasets/init_all_datasets.R")
 source("connection_to_datasets/init_smk_datasets.R")
@@ -24,16 +24,13 @@ connect.smk.dataset.sim(list("LAB_TSC"))
 # Tests
 #
 
-context("ds.matrixDet.report::arg::test errors")
-
-test_that("ds.matixDet.report erros", {
-    res <- ds.matrixDet.report(M1=NULL)
-
-    expect_equal(res, "Error: Please provide the name of the matrix representing M1")
+context("ds.var.o::arg::test errors")
+test_that("var_erros", {
+    expect_error(ds.var.o(), "Please provide the name of the input vector!", fixed=TRUE)
 })
 
 #
-# Tear down
+# Done
 #
 
-# context("dsBetaTestClient::ds.matrixDet.report::arg done")
+# context("dsBetaTestClient::ds.var.o::arg done")
