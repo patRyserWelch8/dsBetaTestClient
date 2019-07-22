be70978dc7616ff78b5fc680bad6bdbb1336b10e
Stuart Wheater <stuart.wheater@arjuna.com>
2019-07-22 15:25:29 +0100

    Argument test fix

--- tests/testthat/test-arg-ds.asFactor.o.R
+++ tests/testthat/test-arg-ds.asFactor.o.R
@@ -28,7 +28,7 @@ context("ds.asFactor.o::arg::test errors")
 test_that("asFactor_erros", {
     expect_error(ds.asFactor.o(), "Please provide the name of the variable that is to be converted to a factor e.g. 'varname'", fixed=TRUE)
     expect_error(ds.asFactor.o(input.var.name=1), "Please provide the name of the variable that is to be converted to a factor in character format e.g. 'varname'", fixed=TRUE)
-    expect_error(ds.asFactor.o(input.var.name=c()), "Please provide the name of the variable that is to be converted to a factor in character format e.g. 'varname'", fixed=TRUE)
+    expect_error(ds.asFactor.o(input.var.name=NA), "Please provide the name of the variable that is to be converted to a factor in character format e.g. 'varname'", fixed=TRUE)
 })
 
 #
