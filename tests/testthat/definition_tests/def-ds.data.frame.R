9f7e18c6474f575d6883d46394869f5f58ba3e5a
redraven <npr69@ncl.ac.uk>
2019-07-25 17:18:47 +0100

    ds.data.frame.o + merge

--- tests/testthat/definition_tests/def-ds.data.frame.R
+++ tests/testthat/definition_tests/def-ds.data.frame.R
@@ -1,17 +1,27 @@
-source("definition_tests/def-assign-stats.R")
+
 .test.data.frame.creation <- function(list.variables,data.frame.name)
 {
-  
-  data.frame.server <- ds.dataFrame.o(x=list.variables, newobj = data.frame.name, datasources = ds.test_env$connection.opal)
- 
-  type <- ds.class(data.frame.name)
-  exists <- ds.exists(data.frame.name, datasources = ds.test_env$connection.opal)
-  
-  for(i in 1:length(exists))
-  {
-    expect_true(exists[[i]])
-  }
-  expect_true(type[[1]][1]=="data.frame")
+     data.frame.server <- ds.dataFrame.o(x=list.variables, newobj = data.frame.name, datasources = ds.test_env$connection.opal)
+     type <- ds.class(data.frame.name)
+     exists <- ds.exists(data.frame.name, datasources = ds.test_env$connection.opal)
+     cols.name <- ds.colnames(data.frame.name,datasources = ds.test_env$connection.opal)
+    
+     expect_true(type[[1]][1]=="data.frame")
+     
+     for(i in 1:length(exists))
+     {
+       expect_true(exists[[i]])
+     }
+    
+     for (server in length(cols.name))
+     {
+        for(i in 1:length(list.variables))
+        {
+          expect_true(cols.name[[server]][i] == list.variables[i])
+        }
+     }
+     
+     
 }
 
 .test.data.frame.from.objects <- function(variable.name,variable.created,data.frame.name)
@@ -32,4 +42,21 @@ source("definition_tests/def-assign-stats.R")
   }
   expect_true(type[[1]][1]=="data.frame")
 }
-#
+
+
+.test.data.frame.from.different.objects <- function(data.frame.name)
+{
+  ds.make.o('D$NUMERIC','numeric_var')
+  ds.make.o('D$FACTOR','factor_var')
+  
+  data.frame.server <- ds.dataFrame.o(x=c('numeric_var','factor_var'), newobj = data.frame.name, datasources = ds.test_env$connection.opal)
+  type <- ds.class(data.frame.name)
+  exists <- ds.exists(data.frame.name, datasources = ds.test_env$connection.opal)
+  
+  for(i in 1:length(exists))
+  {
+    expect_true(exists[[i]])
+  }
+  expect_true(type[[1]][1]=="data.frame")
+  
+}
